3618db453129bd22379789dbb593b62a2a939aa0
root <root@zf2bis.(none)>
2014-12-10 23:27:51 +0000

    Auto commit for MySQL on LXC container zf2bis at Wed Dec 10 23:27:51 UTC 2014

---
+++ mysql/album.sql
@@ -0,0 +1,10 @@
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `album` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `artist` varchar(100) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
